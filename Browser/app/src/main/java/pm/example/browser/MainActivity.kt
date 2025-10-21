66def18c09fbfd778b18e52496e023a47d961391
Bruno Bastos <bruno.bastos@ua.pt>
2025-10-21 12:29:31 +0100

    browser mas sem funcionar

--- Browser/app/src/main/java/pm/example/browser/MainActivity.kt
+++ Browser/app/src/main/java/pm/example/browser/MainActivity.kt
@@ -1,12 +1,17 @@
 package pm.example.browser
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pm.example.browser.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
